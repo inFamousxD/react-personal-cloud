f34924d983fb06b97ebdf6af03fe71c21a33614f
inFamousxD <audimankar@gmail.com>
2021-07-07 19:21:04 +0530

    Cleaned up dash icon styling

--- src/components/home/Dash.js
+++ src/components/home/Dash.js
@@ -9,11 +9,6 @@ const dashButtons = {
 	margin: '0vh 1vw',
 }
 
-const dashButtonSelected = {
-	color: '#CA4246',
-	margin: '0vh 1vw',
-}
-
 const Dash = ({ expandNavbarHandler, selected, setSelected }) => {
 	const handleIndex = (index) => {
 		setSelected(index);
@@ -40,14 +35,17 @@ const Dash = ({ expandNavbarHandler, selected, setSelected }) => {
 		}
 	}, [selected, expandNavbarHandler, dash])
 
+	const buttonTextStyle = { fontWeight: '450', fontSize: '20px', color: '#eee' }
+
 	return (
 		<div>
 		<Card style={dash} variant="outlined">
 			<CardContent>
-				<IconButton onClick={() => {handleIndex(0)}}><DescriptionOutlined style={selected === 0 ? dashButtonSelected : dashButtons}/><span style={{ fontWeight: '450', fontSize: '20px', color: '#eee' }}>{" Notes"}</span></IconButton>
-				<IconButton onClick={() => {handleIndex(1)}}><LinkOutlined style={selected === 1 ? dashButtonSelected : dashButtons}/><span style={{ fontWeight: '450', fontSize: '20px', color: '#eee' }}>{" Links"}</span></IconButton>
-				<IconButton onClick={() => {handleIndex(2)}}><OutlinedFlag style={selected === 2 ? dashButtonSelected : dashButtons}/><span style={{ fontWeight: '450', fontSize: '20px', color: '#eee' }}>{" Goals"}</span></IconButton>
-				<IconButton onClick={() => {handleIndex(3)}}><BookOutlined style={selected === 3 ? dashButtonSelected : dashButtons}/><span style={{ fontWeight: '450', fontSize: '20px', color: '#eee' }}>{" Diary"}</span></IconButton>
+				<IconButton onClick={() => {handleIndex(0)}}><DescriptionOutlined style={dashButtons}/><span style={buttonTextStyle}>{" Notes"}</span></IconButton>
+				<IconButton onClick={() => {handleIndex(1)}}><LinkOutlined style={dashButtons}/><span style={buttonTextStyle}>{" Links"}</span></IconButton>
+				<IconButton onClick={() => {handleIndex(2)}}><OutlinedFlag style={dashButtons}/><span style={buttonTextStyle}>{" Goals"}</span></IconButton>
+				<IconButton onClick={() => {handleIndex(3)}}><BookOutlined style={dashButtons}/><span style={buttonTextStyle}>{" Diary"}</span></IconButton>
+				{/* <IconButton onClick={() => {handleIndex(4)}}><SettingsApplicationsOutlined style={dashButtons}/><span style={buttonTextStyle}>{" Prefs"}</span></IconButton> */}
 			</CardContent>
 		</Card>
 		</div>
