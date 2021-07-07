3fd1277a99dbc2c3a4ddaeb4bfb9f480e8f25acf
inFamousxD <audimankar@gmail.com>
2021-07-07 19:22:07 +0530

    Modified exampled data

--- src/components/notes/notesData.js
+++ src/components/notes/notesData.js
@@ -1,6 +1,6 @@
 const notesDataSet = [
   {
-    "folderName": "dolor ad",
+    "folderName": "Random",
     "notes": [
       {
         "title": "sunt duis eu reprehenderit consectetur deserunt nostrud est",
@@ -89,7 +89,7 @@ const notesDataSet = [
     ]
   },
   {
-    "folderName": "sint mollit",
+    "folderName": "Recently Added",
     "notes": [
       {
         "title": "veniam eu exercitation culpa ullamco in deserunt anim",
@@ -122,7 +122,7 @@ const notesDataSet = [
     ]
   },
   {
-    "folderName": "consequat reprehenderit",
+    "folderName": "Quick Notes",
     "notes": [
       {
         "title": "amet Lorem excepteur nisi enim magna qui elit",
@@ -183,7 +183,7 @@ const notesDataSet = [
     ]
   },
   {
-    "folderName": "duis irure",
+    "folderName": "Persistent",
     "notes": [
       {
         "title": "dolor amet incididunt cupidatat nulla culpa ex in",
@@ -226,342 +226,6 @@ const notesDataSet = [
         "note": "est non aliquip id magna minim et est ut commodo enim reprehenderit cillum",
         "important": true,
         "progress": 0
-      }
-    ]
-  },
-  {
-    "folderName": "incididunt do",
-    "notes": [
-      {
-        "title": "elit elit amet labore laboris occaecat et duis",
-        "date": "1995-06-08T07:42:38.145Z",
-        "note": "id nostrud culpa veniam consequat anim do exercitation nisi velit in",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "dolor est aliquip elit est exercitation esse laborum",
-        "date": "2015-08-23T18:58:24.048Z",
-        "note": "do ut aliqua enim aliqua culpa ex ullamco eu aute nisi sunt deserunt",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "ut id esse Lorem eiusmod cupidatat do labore",
-        "date": "1994-12-26T11:45:33.050Z",
-        "note": "dolor exercitation nostrud nostrud nostrud aute ipsum sunt adipisicing consequat",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "consequat minim nulla laboris enim aute pariatur qui",
-        "date": "1978-09-04T09:04:38.439Z",
-        "note": "enim sit enim occaecat fugiat ipsum ad eiusmod adipisicing sint magna sunt ea culpa tempor eu dolor tempor sint esse ut culpa officia",
-        "important": true,
-        "progress": 0
-      },
-      {
-        "title": "nulla veniam anim sit ipsum sit ad ad",
-        "date": "1981-07-29T19:54:22.340Z",
-        "note": "culpa anim esse in nostrud laboris ea magna sit reprehenderit ea ea consectetur do minim exercitation",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "irure Lorem occaecat magna Lorem proident adipisicing reprehenderit",
-        "date": "2013-10-21T03:22:23.500Z",
-        "note": "id ipsum consequat Lorem adipisicing non culpa ullamco commodo aute amet sint sunt ad tempor eu mollit laborum occaecat anim",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "dolor sit aliqua nulla nostrud duis adipisicing dolore",
-        "date": "1992-12-25T16:42:00.704Z",
-        "note": "Lorem commodo ad ad cupidatat sint sint excepteur adipisicing aliquip nostrud ullamco in et irure ut ut est nostrud duis sunt tempor mollit aliquip",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "enim reprehenderit ullamco sint ullamco velit deserunt eu",
-        "date": "2018-04-27T01:01:12.509Z",
-        "note": "nostrud dolor sit enim cupidatat commodo amet mollit voluptate tempor incididunt sit minim id pariatur ullamco ullamco id irure Lorem est nostrud aliqua consectetur",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "anim sint ad consectetur quis mollit dolore ipsum",
-        "date": "1980-02-06T18:42:51.840Z",
-        "note": "ea anim dolore ipsum est veniam ipsum culpa est commodo consequat anim est magna pariatur ea duis velit eiusmod et",
-        "important": true,
-        "progress": 2
-      }
-    ]
-  },
-  {
-    "folderName": "mollit duis",
-    "notes": [
-      {
-        "title": "commodo amet ea adipisicing Lorem cupidatat incididunt quis",
-        "date": "1994-11-19T04:05:52.183Z",
-        "note": "mollit incididunt ad culpa officia Lorem sit aliquip esse id eiusmod",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "cillum eu laborum ut nisi sint ea nostrud",
-        "date": "1991-04-03T06:06:21.663Z",
-        "note": "enim commodo officia veniam et do sit ipsum nisi enim aute do Lorem aliquip nisi ea aliquip in incididunt reprehenderit in nisi eu",
-        "important": true,
-        "progress": 0
-      },
-      {
-        "title": "velit ipsum et magna dolore est dolor non",
-        "date": "1970-11-26T12:28:26.031Z",
-        "note": "quis ex incididunt irure quis veniam sunt non elit laboris exercitation eu deserunt cupidatat aute in qui nulla consequat",
-        "important": false,
-        "progress": 0
-      }
-    ]
-  },
-  {
-    "folderName": "minim voluptate",
-    "notes": [
-      {
-        "title": "dolor laboris velit Lorem id sint reprehenderit deserunt",
-        "date": "1971-07-04T18:35:49.273Z",
-        "note": "nisi culpa occaecat incididunt nisi amet elit sunt est mollit consequat laboris commodo",
-        "important": true,
-        "progress": 0
-      },
-      {
-        "title": "enim deserunt nulla aliquip nostrud nostrud voluptate irure",
-        "date": "2004-07-01T00:23:35.750Z",
-        "note": "nostrud laboris labore eu pariatur non consequat qui quis nostrud proident Lorem pariatur",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "exercitation in eu qui deserunt nulla nulla ut",
-        "date": "1990-11-01T16:45:29.282Z",
-        "note": "incididunt eu sunt ad velit fugiat laboris excepteur est sunt",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "est voluptate ipsum sunt est non fugiat laborum",
-        "date": "1993-04-09T07:26:19.573Z",
-        "note": "officia ipsum exercitation sint fugiat magna magna enim est et incididunt aliquip do Lorem proident sunt",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "nostrud dolore ex ipsum anim exercitation proident reprehenderit",
-        "date": "2002-05-06T10:06:53.646Z",
-        "note": "pariatur id esse dolore anim laboris elit cupidatat cupidatat voluptate nostrud deserunt proident quis tempor eiusmod sint exercitation aliqua amet minim mollit deserunt",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "consectetur cillum enim aute est id anim ipsum",
-        "date": "2020-08-27T15:46:43.418Z",
-        "note": "quis culpa do aliqua elit irure ipsum officia incididunt consectetur fugiat adipisicing consequat velit elit nisi",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "sunt elit veniam reprehenderit quis enim nostrud excepteur",
-        "date": "2013-06-09T15:38:10.869Z",
-        "note": "commodo proident consectetur minim eu est fugiat veniam tempor ea ex officia pariatur esse laborum est dolor duis pariatur irure amet",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "reprehenderit labore labore laboris ea nulla nostrud cillum",
-        "date": "2010-05-04T16:27:31.220Z",
-        "note": "aliqua ad aute duis officia officia cupidatat exercitation excepteur duis ipsum",
-        "important": true,
-        "progress": 1
-      },
-      {
-        "title": "proident enim in veniam cillum aliquip eu ea",
-        "date": "2008-04-08T07:52:40.414Z",
-        "note": "qui ex irure fugiat deserunt veniam nisi aute id eiusmod occaecat labore nisi labore id aliqua reprehenderit velit cupidatat",
-        "important": true,
-        "progress": 2
-      }
-    ]
-  },
-  {
-    "folderName": "officia incididunt",
-    "notes": [
-      {
-        "title": "eiusmod adipisicing quis id enim laborum ex mollit",
-        "date": "2019-06-12T01:23:59.501Z",
-        "note": "tempor ullamco magna consequat ipsum laborum non laboris consequat exercitation aliqua voluptate commodo deserunt esse aute dolor laboris",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "aliqua elit culpa culpa aliquip anim ex cupidatat",
-        "date": "2018-04-26T20:51:57.416Z",
-        "note": "fugiat do commodo ipsum do voluptate non laborum minim ut",
-        "important": true,
-        "progress": 0
-      },
-      {
-        "title": "duis elit labore tempor incididunt voluptate do sunt",
-        "date": "1974-07-15T19:00:42.351Z",
-        "note": "ad exercitation nisi officia fugiat dolore sunt laboris eu cillum eu id elit",
-        "important": true,
-        "progress": 2
-      }
-    ]
-  },
-  {
-    "folderName": "amet cupidatat",
-    "notes": [
-      {
-        "title": "ullamco excepteur deserunt veniam non reprehenderit veniam officia",
-        "date": "2019-06-15T23:10:16.337Z",
-        "note": "reprehenderit laborum esse consequat minim veniam sunt ut veniam consequat",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "cupidatat reprehenderit ut velit cillum incididunt anim sint",
-        "date": "1974-08-24T13:12:23.167Z",
-        "note": "minim laboris esse id qui anim in dolore id cupidatat laboris reprehenderit",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "id eu laboris eiusmod quis nisi et voluptate",
-        "date": "1996-07-15T15:59:04.431Z",
-        "note": "nulla reprehenderit aliquip et ipsum minim exercitation est in eiusmod fugiat non",
-        "important": false,
-        "progress": 1
-      }
-    ]
-  },
-  {
-    "folderName": "Lorem dolor",
-    "notes": [
-      {
-        "title": "eiusmod ea adipisicing veniam amet enim commodo adipisicing",
-        "date": "1977-06-26T06:26:53.136Z",
-        "note": "ullamco do aliqua pariatur quis incididunt excepteur ullamco officia tempor adipisicing ullamco proident do eiusmod ex ea",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "ea incididunt eiusmod occaecat consectetur tempor eu mollit",
-        "date": "1975-04-06T08:53:18.385Z",
-        "note": "labore sunt tempor do occaecat occaecat veniam ad quis ut eiusmod",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "aute officia velit commodo nisi ipsum aute laboris",
-        "date": "2011-10-06T10:42:09.055Z",
-        "note": "consectetur culpa fugiat eiusmod consequat deserunt consectetur aliquip nostrud eiusmod nulla ullamco id deserunt labore sit",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "commodo exercitation proident laborum nisi ea Lorem laboris",
-        "date": "1974-01-10T16:24:16.921Z",
-        "note": "nisi pariatur aliqua consequat ex elit magna aliquip nulla non quis magna aliquip exercitation velit ipsum enim aliquip nisi veniam quis",
-        "important": true,
-        "progress": 1
-      },
-      {
-        "title": "enim ea pariatur nostrud sunt laboris id laboris",
-        "date": "2021-04-28T17:38:05.604Z",
-        "note": "sunt Lorem deserunt exercitation sunt mollit duis magna amet qui minim sint sit",
-        "important": true,
-        "progress": 1
-      },
-      {
-        "title": "nisi officia consectetur magna labore veniam officia quis",
-        "date": "2010-05-26T21:31:03.771Z",
-        "note": "nostrud qui ut fugiat laboris est pariatur in duis ex est aliquip ut veniam occaecat labore labore laboris ipsum pariatur magna et sint irure",
-        "important": true,
-        "progress": 1
-      },
-      {
-        "title": "laboris enim officia eu in exercitation sit in",
-        "date": "1985-03-31T07:35:14.400Z",
-        "note": "aliqua eu elit occaecat culpa dolore eiusmod culpa nisi minim consectetur et ad enim cupidatat voluptate cillum eu dolor laboris",
-        "important": false,
-        "progress": 1
-      },
-      {
-        "title": "ullamco non aliquip occaecat duis quis ullamco Lorem",
-        "date": "1977-02-26T08:51:24.555Z",
-        "note": "nulla in officia Lorem irure anim commodo fugiat magna nisi aliquip",
-        "important": true,
-        "progress": 0
-      }
-    ]
-  },
-  {
-    "folderName": "eu dolore",
-    "notes": [
-      {
-        "title": "id qui est officia mollit sunt exercitation veniam",
-        "date": "1994-06-19T00:02:07.433Z",
-        "note": "magna et ut veniam ex aliqua nostrud ad pariatur veniam aute sint commodo eu",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "laborum elit aute velit fugiat minim occaecat aliquip",
-        "date": "1976-04-26T16:01:33.824Z",
-        "note": "adipisicing incididunt esse aliqua aliqua dolore consectetur amet ea amet",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "elit eu sit fugiat ut non est ullamco",
-        "date": "2013-07-29T22:18:26.882Z",
-        "note": "excepteur est qui veniam dolor consectetur eu laboris culpa minim voluptate aute tempor adipisicing nostrud do duis cillum amet elit minim sint sint non",
-        "important": false,
-        "progress": 2
-      },
-      {
-        "title": "veniam nulla minim culpa et consectetur dolor culpa",
-        "date": "1971-08-27T03:17:23.459Z",
-        "note": "excepteur nisi aliqua incididunt ex ex non laboris velit ipsum sit commodo exercitation aliqua et dolor ipsum magna commodo",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "enim reprehenderit id labore occaecat dolor esse labore",
-        "date": "1982-04-03T18:49:18.020Z",
-        "note": "nostrud aliqua est voluptate velit cillum occaecat ullamco incididunt quis cillum anim culpa aliqua cillum",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "Lorem tempor amet pariatur incididunt esse do tempor",
-        "date": "1991-06-02T17:30:45.806Z",
-        "note": "ipsum ipsum voluptate ullamco cupidatat sit cupidatat Lorem voluptate laborum est voluptate",
-        "important": true,
-        "progress": 2
-      },
-      {
-        "title": "proident commodo est aliquip ipsum duis aliquip anim",
-        "date": "2016-01-07T07:32:20.100Z",
-        "note": "id labore in eiusmod sit voluptate ex veniam ea id commodo officia velit fugiat mollit aliqua qui tempor adipisicing enim ipsum dolore ex cupidatat",
-        "important": false,
-        "progress": 0
-      },
-      {
-        "title": "anim qui duis pariatur irure ullamco voluptate commodo",
-        "date": "2010-06-02T19:31:08.869Z",
-        "note": "ullamco irure ipsum pariatur Lorem occaecat irure nisi eu aliquip aute tempor eu Lorem consectetur minim laboris Lorem laborum dolor culpa",
-        "important": true,
-        "progress": 2
       }
     ]
   }
