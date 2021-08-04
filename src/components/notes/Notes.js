32c62483aed5fe38436ba65a04487c999c516520
inFamousxD <audimankar@gmail.com>
2021-08-04 12:19:57 +0530

    Mobile note size bump

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -391,7 +391,7 @@ const Notes = ({ projectFirestore }) => {
                             {
                                 notesData.map((note, index) => (
                                     note.folder === folders[openFolder] && <GridListTile key={index} cols={2} rows={1} >
-                                        <Card onClick={() => {handleOpenNote(note)}} style={note.important ? {...gridListCardStyle, borderTop: '1px solid gold', height: '12vh'} : gridListCardStyle}>
+                                        <Card onClick={() => {handleOpenNote(note)}} style={note.important ? {...gridListCardStyle, borderTop: '1px solid gold', height: '13vh'} : gridListCardStyle}>
                                             <CardContent>
                                                 <Row>
                                                     <Col sm={10} xs={10} md={11} lg={11} xl={11}>
